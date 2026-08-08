5042dfbfb0ea7060d0cc98e570d5acfb5b0c16c3
Gustavo Teixeira <homedreeem@gmail.com>
2026-08-08 19:36:14 -0300

    User insertion via POST

--- src/main/java/com/gustavo/workshopmongo/services/UserService.java
+++ src/main/java/com/gustavo/workshopmongo/services/UserService.java
@@ -7,21 +7,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gustavo.workshopmongo.domain.User;
+import com.gustavo.workshopmongo.dto.UserDTO;
 import com.gustavo.workshopmongo.repository.UserRepository;
 import com.gustavo.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
-		 Optional<User> obj = repo.findById(id);
+		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-		} 
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+
 }
