529551be3748dd5b6fa1e3dcb65cbf1077f5c5ea
Gustavo Teixeira <homedreeem@gmail.com>
2026-08-10 17:44:07 -0300

    Simple query with @Query

--- src/main/java/com/gustavo/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/gustavo/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.gustavo.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.gustavo.workshopmongo.domain.Post;
@@ -10,5 +11,9 @@ import com.gustavo.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTile(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
